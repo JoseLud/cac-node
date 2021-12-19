e3fe78cdcde58ed23e1ef03b1da4f348b284756b
Joe Lud <josehl@gmail.com>
2021-12-18 21:45:16 -0300

    Subiendo las modificaciones

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs')
 app.use(expresslayouts)
 
 app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/productos'))
 app.use('/', require('./routes/contacto'))
--- routes/contacto.js
+++ routes/contacto.js
@@ -5,4 +5,10 @@ router.get('/contacto', (req, res) => {
     res.render('contacto')
 })
 
+router.post('/contacto', (req, res) => {
+    // console.log(req.body.nombre)
+    console.log(req.body)
+    res.send('Recibi el formulario ' + req.body.nombre)
+})
+
 module.exports = router
