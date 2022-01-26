95993ce5250123bf463851c2a7f6c1a52d371586
Joe Lud <josehl@gmail.com>
2022-01-25 21:58:17 -0300

    cambio de rutas y vistas

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const { body, validationResult } = require('express-validator')
+
 router.get('/contacto', (req, res) => {
-    res.render('contacto')
+    res.render('contacto', {values: {}})
 })
 
-router.post('/contacto', (req, res) => {
+router.post('/contacto', [
+    body('nombre', 'El nombre tiene que tener 3 caracteres o mas').isLength(3)
+], (req, res) => {
+    const errors = validationResult(req) 
     // console.log(req.body.nombre)
-    console.log(req.body)
+    if (!errors.isEmpty()) {
+        res.render('contacto', { values: req.body, errors: errors.array() })
+    } else {
     res.send('Recibi el formulario ' + req.body.nombre)
+    }
 })
 
 module.exports = router
